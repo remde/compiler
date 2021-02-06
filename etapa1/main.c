b78ea1b45b6090fe37be7dc27068eb7839241c57
Fernando Remde <ffremde@gmail.com>
2021-02-06 19:40:39 -0300

    Add all KWs to main.c switch

--- etapa1/main.c
+++ etapa1/main.c
@@ -35,29 +35,29 @@ int main(int argc, char** argv) {
 
         switch(token)
         {
-            case KW_BYTE: fprintf(stdout,"KW_BYTE at line %d \n", getLineNumber()); break;
+            case KW_CHAR: fprintf(stdout,"KW_CHAR at line %d \n", getLineNumber()); break;
             case KW_INT: fprintf(stdout,"KW_INT at line %d \n", getLineNumber()); break;
-            case KW_FLOAT: fprintf(stdout,"KW_FLOAT at line %d \n", getLineNumber()); break;
+            case KW_BOOL: fprintf(stdout,"KW_BOOL at line %d \n", getLineNumber()); break;
+            case KW_POINTER: fprintf(stdout,"KW_POINTER at line %d \n", getLineNumber()); break;
             case KW_IF: fprintf(stdout,"KW_IF at line %d \n", getLineNumber()); break;
             case KW_THEN: fprintf(stdout,"KW_THEN at line %d \n", getLineNumber()); break;
             case KW_ELSE: fprintf(stdout,"KW_ELSE at line %d \n", getLineNumber()); break;
-            case KW_LOOP: fprintf(stdout,"KW_LOOP at line %d \n", getLineNumber()); break;
-            case KW_LEAP: fprintf(stdout,"KW_LEAP at line %d \n", getLineNumber()); break;
+            case KW_WHILE: fprintf(stdout,"KW_WHILE at line %d \n", getLineNumber()); break;
             case KW_READ: fprintf(stdout,"KW_READ at line %d \n", getLineNumber()); break;
             case KW_RETURN: fprintf(stdout,"KW_RETURN at line %d \n", getLineNumber()); break;
             case KW_PRINT: fprintf(stdout,"KW_PRINT at line %d \n", getLineNumber()); break;
-            case OPERATOR_AND: fprintf(stdout,"OPERATOR_AND at line %d \n", getLineNumber()); break;
-            case OPERATOR_OR: fprintf(stdout,"OPERATOR_OR at line %d \n", getLineNumber()); break;
-            case OPERATOR_NOT: fprintf(stdout,"OPERATOR_NOT at line %d \n", getLineNumber()); break;
             case OPERATOR_LE: fprintf(stdout,"OPERATOR_LE at line %d \n", getLineNumber()); break;
             case OPERATOR_GE: fprintf(stdout,"OPERATOR_GE at line %d \n", getLineNumber()); break;
             case OPERATOR_EQ: fprintf(stdout,"OPERATOR_EQ at line %d \n", getLineNumber()); break;
             case OPERATOR_DIF: fprintf(stdout,"OPERATOR_DIF at line %d \n", getLineNumber()); break;
+            case LEFT_ASSIGN: fprintf(stdout,"LEFT_ASSIGN at line %d \n", getLineNumber()); break;
+            case RIGHT_ASSIGN: fprintf(stdout,"RIGHT_ASSIGN at line %d \n", getLineNumber()); break;
             case TK_IDENTIFIER: fprintf(stdout,"TK_IDENTIFIER at line %d \n", getLineNumber()); break;
             case LIT_INTEGER: fprintf(stdout,"LIT_INTEGER at line %d \n", getLineNumber()); break;
-            case LIT_FLOAT: fprintf(stdout,"LIT_FLOAT at line %d \n", getLineNumber()); break;
             case LIT_CHAR: fprintf(stdout,"LIT_CHAR at line %d \n", getLineNumber()); break;
             case LIT_STRING: fprintf(stdout,"LIT_STRING at line %d \n", getLineNumber()); break;
+            case LIT_TRUE: fprintf(stdout,"LIT_TRUE at line %d \n", getLineNumber()); break;
+            case LIT_FALSE: fprintf(stdout,"LIT_FALSE at line %d \n", getLineNumber()); break;
             case TOKEN_ERROR: fprintf(stdout,"TOKEN_ERROR at line %d \n", getLineNumber()); break;
 
             default: fprintf(stdout,"TOKEN <%c> at line %d \n",token,getLineNumber());
